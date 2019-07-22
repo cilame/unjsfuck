350eb3453bf9de3abcc53b5c7b0c78cf76e15b5e
cilame <opaquism@hotmail.com>
2019-07-22 15:55:27 +0800

    add

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import vrequest
+ 
+setup(
+    name = "unjsfuck",
+    version = '0.0.0',
+    keywords = "unjsfuck",
+    author = "cilame",
+    author_email = "opaquism@hotmail.com",
+    url="https://github.com/cilame/unjsfuck",
+    license = "MIT",
+    description = "",
+    long_description = '',
+    long_description_content_type="text/markdown",
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+    ],
+
+    packages = [
+        "unjsfuck",
+    ],
+    python_requires=">=3.6",
+)
---
+++ unjsfuck/__init__.py
@@ -0,0 +1,1 @@
+from .unjsfuck import unjsfuck
